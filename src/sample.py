1d232af389369d1605082a83c1a7cc1510d7dff4
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2018-09-19 00:58:54 -0500

    added monte carlo method; working on midpoint Latin hypercube sampling

--- src/sample.py
+++ src/sample.py
@@ -1,44 +1,48 @@
 import constraints
+import random
 
-def linspace(length):
-  return [x*(1.0/(length-1)) for x in range(length)]
-
-def get_point_list(point_list, sample_vector, n_dims): 
-  if len(point_list) >= pow(n_dims, n_dims):
-    return point_list
-  else: 
-    new_point_list = list() 
-    for point in point_list:
-      for val in sample_vector:
-        new_point_list += [point + [val]]
-    return get_point_list(new_point_list, sample_vector, n_dims)              
-
-def evaluate(sample_vector, problem_constraints):
-  n_good_results = 0
-  point_list = get_point_list([[x] for x in sample_vector], sample_vector, problem_constraints.get_ndim())
-  print len(point_list)
-  print point_list
-  exit()
-  for point in point_list: 
-    print point
+def get_valid_points_using_monte_carlo(problem_constraints, number_of_points):
+  valid_point_list = []
+  valid_point_counter = 0
+  n_dims = problem_constraints.get_ndim()
+  while valid_point_counter != number_of_points: 
+    point = [random.uniform(0,1) for x in range(n_dims)] 
     if problem_constraints.apply(point):
-      n_good_results += 1
-  return n_good_results
+      valid_point_list.append(point) 
+      valid_point_counter += 1
+  return valid_point_list 
+
+def filter_out_valid_points(point_list, problem_constraints, n_results):
+  for point in point_list:
+    if problem_constraints.apply(point):
+      valid_point_list.append(point)
+  return random.sample(point_list, n_results)
+
+# Divide the hypercube in half until you get number_of_points evenly divided regions 
+def get_division_vector(number_of_points, ndims):
+  number_of_regions = 1
+  dimension_index = 0
+  division_vector = [1]*ndims
+  while total_points < number_of_points:
+    dimension_index = (dimension_index + 1) % ndims
+    division_vector[dimension_index] += 1
+    total_points *= 2 
+  return division_vector
+
+def get_midpoints(division_vector):
+
+
+# Used http://www.lumina.com/blog/latin-hypercube-vs.-monte-carlo-sampling as a reference
+def get_valid_points_using_random_latin_hypercube(problem_constraints, number_of_points):
+  n_dims = problem_constraints.get_ndim()
+  dimension_division_vector = get_division_vector(number_of_points, ndims) 
+  return   
+
+def get_most_scattered_points(points, n_results):
+  return points[:n_results]
 
 def get_samples(input_file, n_results):
   problem_constraints = constraints.Constraint(input_file) 
-  n_dimensions = problem_constraints.get_ndim()
-  n_samples_per_dimension = pow(n_results, (1.0/3))
-  if int(n_samples_per_dimension) < n_samples_per_dimension:
-    n_samples_per_dimension = int(n_samples_per_dimension) + 1
-  n_good_results = 0 
-  
-  while (n_good_results < n_results):  
-    print 'number of samples per dimension ', n_samples_per_dimension
-    # I could use numpy.linspace here, but I did not want a numpy dependency
-    sample_vector = linspace(n_samples_per_dimension) 
-    n_good_results = evaluate(sample_vector, problem_constraints)
-    n_samples_per_dimension = n_samples_per_dimension + 1
-
-  return sample_points[:n_results]
+  valid_points = get_valid_points(problem_constraints, n_results*100)
+  return best_point_set 
 
