1e4ab5adb0173478912a3af7c97c100847d87bad
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2018-09-20 01:41:21 -0500

    backup

--- src/sample.py
+++ src/sample.py
@@ -1,21 +1,36 @@
 import constraints
 import random
 
-def get_boundary_in_direction(start_point, direction, epsilon, problem_constraints):
-  max_boundary = start_point
-  while problem_constraints.apply(max_boundary):   
-    max_boundary[direction] += epsilon
-  min_boundary = start_point
-  while problem_constraints.apply(max_boundary):   
-    max_boundary[direction] -= epsilon
-  return [min_boundary, max_boundary]
 
+def valid(point, problem_constraints):
+  if not problem_constraints.apply(point):
+    return False
+  for val in point:
+    if (val<0.001 or val>1.000):
+      return False
+  return True
+
+# TODO
+def increment_in_direction(point, direction, epsilon):
+  return point
+
+
+def get_boundary_in_direction(start_point, direction, epsilon, problem_constraints):
+  min_boundary = start_point 
+  while valid(min_boundary, problem_constraints):
+    min_boundary = increment_in_direction(min_boundary, direction, -1*epsilon) 
+  max_boundary = start_point 
+  while valid(max_boundary, problem_constraints):
+    max_boundary = increment_in_direction(max_boundary, direction,  epsilon)
+  return [min_boundary[direction], max_boundary[direction]]
+  
 # source: https://mathoverflow.net/questions/9854/uniformly-sampling-from-convex-polytopes
-def find_valid_point(start_point, problem_constraints):
-  direction = random.randint(0,problem_constraints.get_ndim()-1)
-  boundary_points = get_boundary_in_direction(start_point, direction, 0.0001, problem_constraints)
-  start_point[direction] += random.uniform(boundary_points[0], boundary_points[1])   
-  return start_point
+def find_valid_point(valid_point, problem_constraints):
+  direction = random.randint(0, problem_constraints.get_ndim()-1)
+  epsilon = 0.0001
+  boundaries = get_boundary_in_direction(valid_point, direction, epsilon, problem_constraints)
+  valid_point = random.uniform(boundaries[0], boundaries[1])   
+  return valid_point
 
 def evenly_sample_valid_points(problem_constraints, number_of_results):
   n_dims = problem_constraints.get_ndim()
@@ -27,10 +42,11 @@ def evenly_sample_valid_points(problem_constraints, number_of_results):
     valid_point_list.append(valid_point)
     valid_points_obtained += 1
     start_point = valid_point 
-  return evenly_saple_valid_points
+  return valid_point_list 
  
 def get_samples(input_file, n_results):
   problem_constraints = constraints.Constraint(input_file) 
-  points = evenly_sample_valid_points(problem_constraints, n_results*100)
+  points = evenly_sample_valid_points(problem_constraints, n_results)
+  print points
   return points 
 
