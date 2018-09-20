c515c62bc2b8c1f7979c8deccb695b77383cf04f
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2018-09-20 14:32:58 -0500

    final commit before submission

--- src/sample.py
+++ src/sample.py
@@ -1,6 +1,9 @@
 import constraints
 import random
 
+# Utility function to test both constraints in the input file and in the prompt.
+# Constraints.apply is not enough, because it does not consider that the space
+# is limited to the unit hypercube.
 def valid(point, problem_constraints):
   if not problem_constraints.apply(point):
     return False
@@ -23,8 +26,11 @@ def find_valid_point(start_point, direction, epsilon, problem_constraints):
   valid_point_list = []
   while True: 
     counter += 1
-    if counter > pow(10, 6):
-      return find_valid_point(start_point, direction, epsilon*2, problem_constraints)
+    # This parameter is hardcoded due to time limiations. 
+    # With more time I would try to tune it to for efficiency and accuracy.
+    maximum_iterations = 10 
+    if counter > maximum_iterations:
+      return find_valid_point(start_point, direction, epsilon*10, problem_constraints)
     point = increment_in_direction(point, direction, epsilon) 
     if valid(point, problem_constraints):
       valid_point_list.append(point)
@@ -37,19 +43,23 @@ def find_valid_point(start_point, direction, epsilon, problem_constraints):
 def evenly_sample_valid_points(problem_constraints, number_of_results):
   n_dims = problem_constraints.get_ndim()
   example = problem_constraints.get_example()
-  print example
-  valid_point_list = [example] 
-  epsilon = 0.00001 
+  valid_point = example
+  valid_point_list = [valid_point] 
+  # This parameter is hardcoded due to time limiations. 
+  # With more time I would try to tune it to for efficiency and accuracy.
+  epsilon = 0.000001 
   while len(valid_point_list) < number_of_results: 
     direction = [random.uniform(-1,1) for x in range(n_dims)]
-    valid_point = find_valid_point(example, direction, epsilon, problem_constraints)   
-    if valid_point is not None:
-      valid_point_list.append(valid_point)
+    test_point = find_valid_point(valid_point, direction, epsilon, problem_constraints)   
+    if test_point is not None:
+      valid_point_list.append(test_point)
+      # Use new valid point found as the next start point.
+      valid_point = test_point
+    # If a valid point wasn't found, repeat using the same starting point
+    # but a new random direction.
   return valid_point_list 
  
 def samples(input_file, n_results):
   problem_constraints = constraints.Constraint(input_file) 
   points = evenly_sample_valid_points(problem_constraints, n_results)
-  for point in points:
-    print point
   return points 
