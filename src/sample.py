9679eb3bedd4c9f859918ec303b97965b486cbe9
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2018-09-19 23:50:09 -0500

    replaced monte carlo with hit and run algorithm

--- src/sample.py
+++ src/sample.py
@@ -1,48 +1,36 @@
 import constraints
 import random
 
-def get_valid_points_using_monte_carlo(problem_constraints, number_of_points):
-  valid_point_list = []
-  valid_point_counter = 0
+def get_boundary_in_direction(start_point, direction, epsilon, problem_constraints):
+  max_boundary = start_point
+  while problem_constraints.apply(max_boundary):   
+    max_boundary[direction] += epsilon
+  min_boundary = start_point
+  while problem_constraints.apply(max_boundary):   
+    max_boundary[direction] -= epsilon
+  return [min_boundary, max_boundary]
+
+# source: https://mathoverflow.net/questions/9854/uniformly-sampling-from-convex-polytopes
+def find_valid_point(start_point, problem_constraints):
+  direction = random.randint(0,problem_constraints.get_ndim()-1)
+  boundary_points = get_boundary_in_direction(start_point, direction, 0.0001, problem_constraints)
+  start_point[direction] += random.uniform(boundary_points[0], boundary_points[1])   
+  return start_point
+
+def evenly_sample_valid_points(problem_constraints, number_of_results):
   n_dims = problem_constraints.get_ndim()
-  while valid_point_counter != number_of_points: 
-    point = [random.uniform(0,1) for x in range(n_dims)] 
-    if problem_constraints.apply(point):
-      valid_point_list.append(point) 
-      valid_point_counter += 1
-  return valid_point_list 
-
-def filter_out_valid_points(point_list, problem_constraints, n_results):
-  for point in point_list:
-    if problem_constraints.apply(point):
-      valid_point_list.append(point)
-  return random.sample(point_list, n_results)
-
-# Divide the hypercube in half until you get number_of_points evenly divided regions 
-def get_division_vector(number_of_points, ndims):
-  number_of_regions = 1
-  dimension_index = 0
-  division_vector = [1]*ndims
-  while total_points < number_of_points:
-    dimension_index = (dimension_index + 1) % ndims
-    division_vector[dimension_index] += 1
-    total_points *= 2 
-  return division_vector
-
-def get_midpoints(division_vector):
-
-
-# Used http://www.lumina.com/blog/latin-hypercube-vs.-monte-carlo-sampling as a reference
-def get_valid_points_using_random_latin_hypercube(problem_constraints, number_of_points):
-  n_dims = problem_constraints.get_ndim()
-  dimension_division_vector = get_division_vector(number_of_points, ndims) 
-  return   
-
-def get_most_scattered_points(points, n_results):
-  return points[:n_results]
-
+  valid_point = problem_constraints.get_example()
+  valid_point_list = [valid_point] 
+  valid_points_obtained = 0 
+  while valid_points_obtained < number_of_results:
+    valid_point = find_valid_point(valid_point, problem_constraints)   
+    valid_point_list.append(valid_point)
+    valid_points_obtained += 1
+    start_point = valid_point 
+  return evenly_saple_valid_points
+ 
 def get_samples(input_file, n_results):
   problem_constraints = constraints.Constraint(input_file) 
-  valid_points = get_valid_points(problem_constraints, n_results*100)
-  return best_point_set 
+  points = evenly_sample_valid_points(problem_constraints, n_results*100)
+  return points 
 
